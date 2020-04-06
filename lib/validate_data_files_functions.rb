9bc18f634a71db779b88d10244bc8a89a347d3e7
Travis Wiltshire <wiltshp@gmail.com>
2020-04-06 12:38:44 -0400

    Update validate_data_files_functions.rb

--- lib/validate_data_files_functions.rb
+++ lib/validate_data_files_functions.rb
@@ -66,8 +66,8 @@ def load_and_validate_data
   end
 
   query=("SELECT min(`game_date`) AS 'min' FROM `raw_data_table` LIMIT 1")
-  puts("'"@database_handle.execute(query)"'")
-  first_thur = Date.parse @database_handle.execute(query)
+  puts(@database_handle.execute(query).to_s)
+  first_thur = Date.parse @database_handle.execute(query).to_s
   
   query=("SELECT max(`game_date`) AS 'max' FROM `raw_data_table` LIMIT 1")
   last_thur = Date.parse @database_handle.execute(query)
